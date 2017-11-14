0c923c3fa4d6a7a23d0bc936fab90dccabe3d8e3
loc8chris <loc8chris@gmail.com>
2017-11-14 13:03:53 -0600

    changed the contact.php email

--- inc/contact.php
+++ inc/contact.php
@@ -8,7 +8,7 @@ header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
-$admin_email = 'YOUR.EMAIL@MAIL.COM'; // Your Email
+$admin_email = 'me@chrispuente.com'; // Your Email
 $message_min_length = 5; // Min Message Length
 
 
